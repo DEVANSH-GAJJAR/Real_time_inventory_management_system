04d0b087a4846d55057ccaa5a9c502c741ac0ac7
DEVANSH GAJJAR <162860692+DEVANSH-GAJJAR@users.noreply.github.com>
2025-11-05 12:55:03 +0530

    Add processTransactions and getStockAlerts functions
    Implement functions to process transactions and get stock alerts.

---
+++ src/solution.py
@@ -0,0 +1,39 @@
+# The Required Psudo-code or The Functions for the task
+
+import numpy as np 
+import pandas as pd 
+import LinearRegression
+import calendar
+# TASK-1 
+# processTransactions 
+def processTransactions():
+    data = data.json 
+    print(data.inventory, data.transactions)
+    data.purchase = purchase  # type: ignore
+    data.update()
+    
+    if data!=0:
+        return 0
+    else:
+     
+     return data
+#TASK-2 
+#getStockAlerts
+def AddStocks(n):
+   return n + 200
+def getStockAlerts(A,B,C,D,E,days):
+   data = data.json
+   if A.data <= 50:
+      return    
+   if B.data <= 40:
+      return
+   if C.data <= 100:
+      return
+   if D.data <= 25:
+      return 
+   if E.data <= 60:
+      return 
+    
+    #example usage 
+getStockAlerts()
+AddStocks()
